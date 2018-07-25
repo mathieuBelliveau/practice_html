b12b088316fc8c0ae1b7ee9eeb229fe085636364
Mathieu-Home <35114997+magnum-it@users.noreply.github.com>
2018-07-25 12:02:14 -0300

    JS Experiment

---
+++ themes.js
@@ -0,0 +1,14 @@
+$(document).ready(
+$('#winter').click(function(){
+    var val1 = $(this).html();
+    if(value=='Winter'){
+        $(this).html("Hello, World!");
+//        $(this).html('Small'); 
+//        $('div').css('width',"100%");
+    }else{
+//    $(this).html('Big');
+//    $('div').css('width',"50%");
+    }
+    
+})
+)
